03dc5a1814e435803f6408479dd0346d29dc441e
malekronaldo064 <m.toukebri@cfa-insta.fr>
2023-11-22 16:07:27 +0100

    style page resto list

--- src/app/resto/resto.module.ts
+++ src/app/resto/resto.module.ts
@@ -7,9 +7,11 @@ import { IonicModule } from '@ionic/angular';
 import { RestoPageRoutingModule } from './resto-routing.module';
 
 import { RestoPage } from './resto.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
     FormsModule,
     IonicModule,
